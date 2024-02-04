234f98e412cf0a44d0efce38dc53e7c7dc58206b
Štěpán Velínský <stteranv@gmail.com>
2024-02-05 00:47:39 +0100

    Styling

--- src/components/movie.js
+++ src/components/movie.js
@@ -3,12 +3,12 @@ import data from "../data"
 
 const movie = () => {
 
-    return <div>
+    return <div className = "all-movies">
         {
             data.map( (oneMovie) => {
                 const {id, image, title, age, tags, description} = oneMovie
 
-                return <div key={id}>
+                return <div key={id} className = "one-movie">
                     <img src={image} alt="" />
                     <h2>{title}</h2>
                     <p>{age}</p>
